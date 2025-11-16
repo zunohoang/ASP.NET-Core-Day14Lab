3090af2d0351b9ca3fea0cf339ff7ad7618f4bc9
Nguyen Van Hoang <nguyenvanhoang2005nt@gmail.com>
2025-11-16 19:05:24 +0700

    fwar

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day14Lab_231230949_15_11_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
